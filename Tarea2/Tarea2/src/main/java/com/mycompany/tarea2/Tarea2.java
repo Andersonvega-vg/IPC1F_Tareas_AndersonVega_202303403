396ad2bcd645319cc82321e077ed539a6a9266ba
Anderson Vega <andersonvega.vg@gmail.com>
2026-08-06 11:50:46 -0600

    Creando proyecto Java llamado Tarea2

---
+++ Tarea2/Tarea2/src/main/java/com/mycompany/tarea2/Tarea2.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.tarea2;
+
+/**
+ *
+ * @author ander
+ */
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        //creando primer commit
+        System.out.println("Hola Tarea2");
+    }
+}
