b84826c090995e09373923a3a864f99bd951f74a
Anderson Vega <andersonvega.vg@gmail.com>
2026-08-07 11:53:16 -0600

    Creé estructura de métodos de validación, registro y resultaods

--- Tarea2/src/main/java/com/mycompany/tarea2/Tarea2.java
+++ Tarea2/src/main/java/com/mycompany/tarea2/Tarea2.java
@@ -8,18 +8,27 @@ import java.util.Scanner;
  * @author ander
  */
 public class Tarea2 {
-//REGISTRO DE JUGADORES PARA UN PARTIDO DE FUTBOL MIXTO
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        
-    int numeroCamiseta = 0;
-    int edad = 0; 
-    double altura = 0.0;
-    char genero = ' ';
-    boolean registro = true;
+    //Variables globales
+    static int numeroCamiseta = 0; 
+    static int edad = 0; 
+    static double altura = 0.0;
+    static char genero = ' ';
+    static boolean registro = true;
+    static Scanner sc = new Scanner(System.in);
     
-        System.out.print("Ingrese el numero de camiseta: ");
+    public static void main(String[] args) {
+    //REGISTRO DE JUGADORES PARA UN PARTIDO DE FUTBOL MIXTO
+        leerDatos();
+        validarDatos();
+        mostrarRegistro();
+    }
+    
+    public static void leerDatos(){
+        /*
+        Se encarga de leer datos
+        Ingresados por el ususario    
+        */
+     System.out.print("Ingrese el numero de camiseta: ");
         numeroCamiseta = sc.nextInt();
         
         System.out.print("Ingrese la edad: ");
@@ -30,9 +39,14 @@ public class Tarea2 {
         
         System.out.print("Ingrese el genero H(hombre)/M(mujer): ");
         genero = sc.next().charAt(0);
-        
-        //VALIDACIONES
-        if (numeroCamiseta <1 || numeroCamiseta >99) {
+    }
+    public static void validarDatos(){
+         /*
+        valida que el número de camiseta, la edad,
+        la estatura y el género cumplan con las reglas
+        */
+         registro = true;
+            if (numeroCamiseta <1 || numeroCamiseta >99) {
           registro = false;  
         }
         if (edad <18 || edad >60) {
@@ -44,13 +58,18 @@ public class Tarea2 {
           if (genero != 'H' && genero != 'M') {
             registro = false;
         }
-          if (registro == true) {
-              System.out.println("=====Registro Exitoso=====");
+          
+    }
+    
+    public static void mostrarRegistro(){
+        /*
+        Muestra el resultado del registro dependiendo 
+        de los datos ingresados por el usuario
+        */
+    if (registro == true) {
+              System.out.println("===== Registro Exitoso =====");
         }else{
-              System.out.println("=====Datos no validos=====");
+              System.out.println("===== Datos no validos =====");
           }
-      
-
-    //FIN MAIN      
     }
 }
