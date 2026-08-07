8f28b44c97e22fcdf168b7aad630d64f88e38ff2
Anderson Vega <andersonvega.vg@gmail.com>
2026-08-06 21:05:56 -0600

    Se creo la idea base del programa y bases de validaciónes

--- Tarea2/src/main/java/com/mycompany/tarea2/Tarea2.java
+++ Tarea2/src/main/java/com/mycompany/tarea2/Tarea2.java
@@ -1,14 +1,50 @@
 
 package com.mycompany.tarea2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ander
  */
 public class Tarea2 {
-
+//REGISTRO DE JUGADORES PARA UN PARTIDO DE FUTBOL MIXTO
     public static void main(String[] args) {
-        //creando primer commit
-        System.out.println("Hola Tarea2");
+        
+        Scanner sc = new Scanner(System.in);
+        
+    int numeroCamiseta = 0;
+    int edad = 0; 
+    double altura = 0.0;
+    char genero = ' ';
+    boolean registro = true;
+    
+        System.out.println("Ingrese el número de camiseta");
+        numeroCamiseta = sc.nextInt();
+        
+        System.out.println("Ingrese la edad");
+        edad = sc.nextInt();
+        
+        System.out.println("Ingrese la altura (m)");
+        altura = sc.nextDouble();
+        
+        System.out.println("Ingrese el género H(hombre)/M(mujer)");
+        genero = sc.next().charAt(0);
+        
+        //VALIDACIONES
+        if (edad >= 18 && edad <= 60) {
+            registro = true;
+            System.out.println("Edad válida");
+        }else{
+            registro = false;
+            System.out.println("Edad no válida");
+        }
+        
+        if (genero == 'H' || genero == 'M') {
+            System.out.println("Genero válido");
+        }else{
+            System.out.println("Género no valido");
+        }
+                
     }
 }
